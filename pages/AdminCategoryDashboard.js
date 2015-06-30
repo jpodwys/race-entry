52f16522aa5d4d578679790984ae8b0a4fcfd06b
Joseph Lokahi Podwys <jpodwys@ldschurch.org>
2015-06-30 13:04:15 -0600

    Adding additional page object files.

---
+++ pages/AdminCategoryDashboard.js
@@ -0,0 +1,12 @@
+function AdminCategoryDashboard() {
+  // this.button_createRaceForm = element(by.css('[data-test="create-a-race"]'));
+  // this.button_createCalendarListing = element(by.linkText('Create Race Registration Form'));
+  // this.button_createRaceMap = element(by.linkText('Create Race Registration Form'));
+
+  // this.fetch = function (prefix) {
+  //   browser.get(prefix + 'user');
+  //   return this;
+  // }
+}
+
+module.exports = AdminCategoryDashboard;
