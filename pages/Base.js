bf21a0b31b1c1a1662ba4153848f6e5ce89a71c0
Joseph Lokahi Podwys <jpodwys@ldschurch.org>
2015-07-29 17:18:17 -0600

    Making tests run locally.

--- pages/Base.js
+++ pages/Base.js
@@ -1,8 +1,8 @@
 function Base() {
   //THIS LINE SHOULD BE USED WHEN THE PRODUCT IS DELIVERED
-  //this.basePath = (process.argv.length > 3 && process.argv[3] === '--v2') ? 'http://v2.raceentry.com/' : 'localhost:5000/';
+  this.basePath = (process.argv.length > 3 && process.argv[3] === '--v2') ? 'http://v2.raceentry.com/' : 'dev.raceentry';
   //THIS LINE IS JUST FOR MY PERSONAL DEVELOPMENT
-  this.basePath = (process.argv.length > 3 && process.argv[3] === '--localhost') ? 'localhost:5000/' : 'http://v2.raceentry.com/';
+  //this.basePath = (process.argv.length > 3 && process.argv[3] === '--localhost') ? 'localhost:5000/' : 'http://v2.raceentry.com/';
 }
 
 module.exports = Base;
