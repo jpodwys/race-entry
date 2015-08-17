4d3e052c5f34e131f11886851d4d6ca99bdf2703
Joseph Lokahi Podwys <jpodwys@ldschurch.org>
2015-08-17 13:55:55 -0600

    Adding more descriptive file names and behaviors.

---
+++ conf-sequential.js
@@ -0,0 +1,15 @@
+exports.config = {
+  seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
+  jasmineOpts: {
+    defaultTimeoutInterval: 450000
+  },
+  capabilities: {
+    browserName: 'chrome',
+    shardTestFiles: true,
+    maxInstances: 1
+  },
+  specs: [
+    'tests/CreateRaceIndividual.js',
+    'tests/CreateRaceTeam.js'
+  ]
+};
