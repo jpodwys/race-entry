f87cd09ead0cd0a7be2f97edf964970d54636e3f
Joseph Podwys <joseph.podwys@familysearch.org>
2016-01-25 11:48:34 -0700

    Updating dev URL based on latest developer practices.

--- pages/Base.js
+++ pages/Base.js
@@ -1,5 +1,5 @@
 function Base() {
-  this.basePath = (process.argv.length > 3 && process.argv[3] === '--v2') ? 'http://v2.raceentry.com/' : 'http://dev.raceentry/';
+  this.basePath = (process.argv.length > 3 && process.argv[3] === '--v2') ? 'http://v2.raceentry.com/' : 'http://127.0.0.1:80/';
 }
 
 module.exports = Base;
